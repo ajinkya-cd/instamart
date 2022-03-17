b173e2b9ae8db6d6e2934693c6608618996fb25d
ajinkya-cd <ajinkya.gaikwad@coditas.com>
2022-03-17 12:53:37 +0530

    Added product entity and model

---
+++ lib/data/models/ProductModel.dart
@@ -0,0 +1,49 @@
+import '../../domain/entities/ProductEntity.dart';
+
+class ProductModel extends ProductEntity {
+  ProductModel({
+    String? id,
+    String? title,
+    double? price,
+    String? description,
+    String? category,
+    String? image,
+    double? rating,
+    double? count
+  }) : super(
+      id: id,
+      title: title,
+      price: price,
+      description: description,
+      category: category,
+      image: image,
+      rating: rating,
+      count: count
+  );
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) {
+    return ProductModel(
+        id: json['id'],
+        title: json['title'],
+        price: json['price'],
+        description: json['description'],
+        category: json['category'],
+        image: json['image'],
+        rating: json['rating'],
+        count: json['count'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'price': price,
+      'description': description,
+      'category': category,
+      'image': image,
+      'rating': rating,
+      'count': count,
+    };
+  }
+}
